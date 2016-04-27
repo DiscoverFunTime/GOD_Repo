5a8320ba3ec355b21118225815b563f8a1a86c93
Billy Everyteenkinga.rutynowski@gmail.com <kinga.rutynowski@gmail.com>
2016-04-27 16:10:09 -0700

    added a block navbar to keep navbar acess on some pages, pleas note if you do not want the nav bar on a page: reference bloc navbar to prevent the defult on a jade page

--- public/js/index_landing.js
+++ public/js/index_landing.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   console.log("test")
-   $('#load').fadeOut(4000, function(){ 
-  $('#landing').css('visibility','visible').hide().fadeIn('slow');
-});
+  $('#load').fadeOut(4000, function(){ 
+    $('#landing').css('visibility','visible').hide().fadeIn('slow');
+  });
 
 });
 
