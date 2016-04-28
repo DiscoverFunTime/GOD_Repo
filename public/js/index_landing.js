c43215b430601e2742a209abb5039aa3ef671624
Billy Everyteenkinga.rutynowski@gmail.com <kinga.rutynowski@gmail.com>
2016-04-27 18:57:10 -0700

    got the scroll implemented on the landing page with the logo div

--- public/js/index_landing.js
+++ public/js/index_landing.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   console.log("test")
   $('#load').fadeOut(4000, function(){ 
-    $('#landing').css('visibility','visible').hide().fadeIn('slow');
+    // $('#landing').css('visibility','visible').hide().fadeIn('slow');
   });
 });
 
+//need on click to target the images inside the div 
+//on click on image 
